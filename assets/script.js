035ab7cd9b64b0e767162d696fa9a7f1a84e4964
Laura Beutler <labeutler@gmail.com>
2022-10-18 16:43:02 -0400

    added notes to javascript info.

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,16 @@
 // Reference of DOM elements
 
 
-// display day and date
+// DISPLAY DAY AND DATE
 var weekDay = moment().format("dddd, MMMM Do");
 $("#current").text(weekDay);
 
-//add event listener to save buttons
+//ADD EVENT LISTENER TO SAVE BUTTONS
 var buttons = document.querySelectorAll(".fa-save")
 for(var i=0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", saveNote)
 }
+// ALLOW FOR TEXT TO BE SAVED TO LOCAL STORAGE FROM DAILY CALENDAR WITH SETITEM
 function saveNote(event){
     console.log(event)
     var input = event.target.attributes[0].value
@@ -17,6 +18,7 @@ function saveNote(event){
     console.log(inputValue)
     localStorage.setItem(input, inputValue)
 }
+//RETRIEVING INFORMATION FROM LOCAL STORAGE WITH GETITEM, CONNECTING HOURS WITH CURRENT TIME FOR PAST, PRESENT AND FUTURE.
 for(var i=9; i < 18; i++){
     document.querySelector(".text"+ i).value = localStorage.getItem("text"+ i)
     var timeBlock=document.querySelector(".text"+ i)
